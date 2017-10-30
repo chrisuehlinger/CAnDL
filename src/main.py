3aeecd529b70bf7008b1c21e2047233128b6a592
Chris Uehlinger <chris.uehlinger@gmail.com>
2017-10-30 17:21:56 -0400

    move getFrame function into the recognition module

--- src/main.py
+++ src/main.py
@@ -1,18 +1,5 @@
-from recognition import corners
-import cv2
+from recognition import getFrame
 from time import time
-
-def getFrame(debug=False):
-    cam = cv2.VideoCapture(0)
-    s, im = cam.read() # captures image
-    if debug:
-        cv2.imshow("Test Picture", im) # displays captured image
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    cv2.imwrite("test.png",im)
-    corners('test.png', debug=debug)
-    #corners('../test-images/messy-scene-color.png')
 
 t0 = time()
 getFrame()
--- src/recognition/__init__.py
+++ src/recognition/__init__.py
@@ -1,1 +1,14 @@
 from corners import corners
+import cv2
+
+def getFrame(debug=False):
+    cam = cv2.VideoCapture(0)
+    s, im = cam.read() # captures image
+    if debug:
+        cv2.imshow("Test Picture", im) # displays captured image
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    cv2.imwrite("test.png",im)
+    corners('test.png', debug=debug)
+    #corners('../test-images/messy-scene-color.png')
